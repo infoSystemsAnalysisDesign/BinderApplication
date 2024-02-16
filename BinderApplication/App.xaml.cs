6039368710685c85d9b0d6af95c7d48bfd1df55b
Madelyn Little <madlitt2020@gmail.com>
2024-02-15 23:42:07 -0600

    created Handlers folder that hold BorderlessEntry Implemented SignIn and SignUp pages Altered App shell

--- BinderApplication/App.xaml.cs
+++ BinderApplication/App.xaml.cs
@@ -1,12 +1,27 @@
-﻿namespace BinderApplication
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿using Microsoft.Maui.Platform;
+using BinderApplication.Handlers;
 
-        //    MainPage = new AppShell();
-        }
+namespace BinderApplication;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderlessEntry), (handler, view) =>
+        {
+            if (view is BorderlessEntry)
+            {
+#if __ANDROID__
+                handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+#elif __IOS__
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+            }
+        });
+
+
+        MainPage = new AppShell();
     }
 }
