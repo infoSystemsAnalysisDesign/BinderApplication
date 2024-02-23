1df0122e39a6308ff3fe6f23e0013cadb941b502
Tyler Peaster <tylerpeaster@gmail.com>
2024-02-23 14:50:31 -0600

    Added database connectivity to this branch

--- BinderApplication/App.xaml.cs
+++ BinderApplication/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Maui.Platform;
 using BinderApplication.Handlers;
+using BinderApplication.Database;
 
 namespace BinderApplication;
 
@@ -8,6 +9,8 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+
+        DatabaseConnection databaseConnection = new DatabaseConnection();
 
         Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(BorderlessEntry), (handler, view) =>
         {
