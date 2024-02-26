e03c49055d06f86be18daf2a62f1f0a4d633c1e6
Lindsey Davenport <ldavenport2@atu.edu>
2024-02-26 15:08:44 -0600

    I did it :)

--- BinderApplication/MainPage.xaml.cs
+++ BinderApplication/MainPage.xaml.cs
@@ -1,26 +1,28 @@
 ﻿namespace BinderApplication
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : Shell
     {
-        int count = 0;
+        // int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
+
+    //    private void OnCounterClicked(object sender, EventArgs e)
+    //    {
+    //        count++;
+
+    //        if (count == 1)
+    //            CounterBtn.Text = $"Clicked {count} time";
+    //        else
+    //            CounterBtn.Text = $"Clicked {count} times";
+
+    //        SemanticScreenReader.Announce(CounterBtn.Text);
+    //    }
+    //}
 }
 
 // Alex test comment 11/15/23 on MainPage.xaml.cs
