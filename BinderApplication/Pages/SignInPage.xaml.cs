52cef0ee664ccffa4b55d1f5c9975bdddeda9cc8
Lindsey Davenport <ldavenport2@atu.edu>
2024-02-29 23:42:24 -0600

    Fixing sign in bugs (it was slow for me but it worked, i think it was just emulator)

--- BinderApplication/Pages/SignInPage.xaml.cs
+++ BinderApplication/Pages/SignInPage.xaml.cs
@@ -11,35 +11,47 @@ public partial class SignInPage : ContentPage
 	{
         InitializeComponent();
 	}
-	private async void OnSignInButtonClicked(object sender, EventArgs e)
-	{
-
-
-        //add stuff to make sure has account 
-        // Get a reference to the Users collection
-        var dbConnection = DatabaseConnection.Instance;
-        var client = new MongoClient("mongodb://Binder:AlsoBinder1@ac-clelo6g-shard-00-00.ibrxa6e.mongodb.net:27017,ac-clelo6g-shard-00-01.ibrxa6e.mongodb.net:27017,ac-clelo6g-shard-00-02.ibrxa6e.mongodb.net:27017/?ssl=true&replicaSet=atlas-i5m36b-shard-0&authSource=admin&retryWrites=true&w=majority");
-        var database = client.GetDatabase("Binder");
-
-        // Get a reference to the Users collection
-        var usersCollection = database.GetCollection<BsonDocument>("Login");
-
-        // Check if there is a user with the provided email and password
-        var filter = Builders<BsonDocument>.Filter.Eq("Email", email.Text) & Builders<BsonDocument>.Filter.Eq("Password", password.Text);
-        var user = await usersCollection.Find(filter).FirstOrDefaultAsync();
-
-        if (user != null)
+    private async void OnSignInButtonClicked(object sender, EventArgs e)
+    {
+        try
         {
-            // User found, navigate to MainPage
-            await Navigation.PushAsync(new MainPage());
+            var dbConnection = DatabaseConnection.Instance;
+            var client = new MongoClient("mongodb://Binder:AlsoBinder1@ac-clelo6g-shard-00-00.ibrxa6e.mongodb.net:27017,ac-clelo6g-shard-00-01.ibrxa6e.mongodb.net:27017,ac-clelo6g-shard-00-02.ibrxa6e.mongodb.net:27017/?ssl=true&replicaSet=atlas-i5m36b-shard-0&authSource=admin&retryWrites=true&w=majority");
+            var database = client.GetDatabase("Binder");
+
+            // Get a reference to the Users collection
+            var usersCollection = database.GetCollection<BsonDocument>("Login");
+
+            // Validate user inputs
+            if (string.IsNullOrEmpty(email.Text) || string.IsNullOrEmpty(password.Text))
+            {
+                // Handle invalid inputs, show an alert or display a message to the user
+                return;
+            }
+
+            // Check if there is a user with the provided email and password
+            var filter = Builders<BsonDocument>.Filter.Eq("Email", email.Text) & Builders<BsonDocument>.Filter.Eq("Password", password.Text);
+            var user = await usersCollection.Find(filter).FirstOrDefaultAsync();
+
+            if (user != null)
+            {
+                // User found, navigate to MainPage
+
+                App.Current.MainPage = new MainPage();
+
+            }
+            else
+            {
+                await DisplayAlert("Login Failed", "Invalid email or password", "OK");
+            }
         }
-        else
+        catch (Exception ex)
         {
-            // User not found or incorrect credentials
-            // Handle the case where login fails, show an alert or display a message to the user
+            Console.WriteLine($"Exception: {ex}");
+            // Handle or log the exception accordingly
         }
-        //  await Navigation.PushAsync(new MainPage());
     }
+
 
     private async void TapGestureRecognizer_Tapped_For_SignUP(object sender, TappedEventArgs e)
     {
