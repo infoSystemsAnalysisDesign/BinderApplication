801fea7dda905a1b695c1667fb277ea51d700bbb
Tyler Peaster <tylerpeaster@gmail.com>
2024-03-19 15:12:59 -0500

    Only 40 books are called at a time now

--- BinderApplication/Database/DatabaseBook.cs
+++ BinderApplication/Database/DatabaseBook.cs
@@ -13,22 +13,26 @@ namespace BinderApplication.Database
     {
         private readonly IMongoDatabase database;
 
+        //Determines the amount of books that can be pulled at once
+        private readonly int swipeLimit = 40;
+
         public DatabaseBook()
         {
             this.database = DatabaseConnection.Instance.GetDatabase();
         }
 
         public List<BookModel> RetrieveBooksFromDatabase()
-        {
+{
             string genre = "Fiction";
 
             var collection = database.GetCollection<BsonDocument>("Books-" + genre);
             var filter = Builders<BsonDocument>.Filter.Empty;
-            var books = collection.Find(filter).ToList();
+            var books = collection.Find(filter).Limit(swipeLimit).ToList();
             var bsonString = books.ToJson();
             var cleanedBsonString = BsonStringCleaner.CleanBsonString(bsonString);
             return DeserializeBooks(cleanedBsonString);
         }
+
 
         public static List<BookModel> DeserializeBooks(string jsonString)
         {
