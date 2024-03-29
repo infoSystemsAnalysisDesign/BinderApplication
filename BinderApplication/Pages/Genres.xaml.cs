a651b2e08f5994836dd1eb2deb606182a731eab9
Tyler Peaster <tylerpeaster@gmail.com>
2024-03-29 16:01:28 -0500

    Progress on genre switching

--- BinderApplication/Pages/Genres.xaml.cs
+++ BinderApplication/Pages/Genres.xaml.cs
@@ -1,14 +1,83 @@
 using Microsoft.Maui.Graphics;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
 namespace BinderApplication.Pages;
 
 public partial class Genres : ContentPage
 {
-	public Genres()
+    //Why C# gotta be quirky and call Hashmaps dictionaries
+    Dictionary<string, bool> stringBooleanMap;
+    public Genres()
 	{
 		InitializeComponent();
-       
-	}
-   
 
-  
+        //Hashmap to store whether a switch is flipped on or off
+        stringBooleanMap = new Dictionary<string, bool>();
+
+        /*
+         * We are going to need a way to read in the hashmap values
+         * from the database.
+         * We can then use those values to have the toggles that should be on be on already
+         * Example: Drama.IsToggled = true;
+         * I need to make it so you are given a default hashmap of all false values tied
+         * to your account at account creation.
+         * This will require gettin grid of all current accounts :(
+         * If genres < 4 (so it covers below min genres and when 
+         * account is first created with all false) Have the app pull up a modified
+         * version of the genre page that requires you to pick at leats 4 genres.
+         * The genres page will also need to read in the hashmap values.
+         */
+
+        Drama.IsToggled = true;
+
+        SaveButton.Clicked += OnSaveButtonClicked;
+
+        Drama.Toggled += OnSwitchToggle;
+        Essay.Toggled += OnSwitchToggle;
+        Fiction.Toggled += OnSwitchToggle;
+        History.Toggled += OnSwitchToggle;
+        Horror.Toggled += OnSwitchToggle;
+        NonFiction.Toggled += OnSwitchToggle;
+        Novel.Toggled += OnSwitchToggle;
+        Philosophy.Toggled += OnSwitchToggle;
+        Poetry.Toggled += OnSwitchToggle;
+        Politics.Toggled += OnSwitchToggle;
+        Psychology.Toggled += OnSwitchToggle;
+        Romance.Toggled += OnSwitchToggle;
+        Science.Toggled += OnSwitchToggle;
+        Spirituality.Toggled += OnSwitchToggle;
+        Suspense.Toggled += OnSwitchToggle;
+        Thriller.Toggled += OnSwitchToggle;
+    }
+
+    void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+        //CREATE LOGIC CODE
+        DisplayAlert("Success", "Save Success!", "OK");
+
+        /*
+         * if (genres < 4)
+         * DisplayAlert("Oops", "Please select at least 4 genres!", "OK");
+        */
+    }
+
+    void OnSwitchToggle(object sender, EventArgs e)
+    {
+        string switchName;
+        var switchSender = (Microsoft.Maui.Controls.Switch)sender;
+
+        if (switchSender.IsToggled)
+        {
+            DisplayAlert("It worked", "Switch is Toggled ON", "OK");    //testing
+            switchName = switchSender.AutomationId;
+            stringBooleanMap[switchName] = true;
+        }
+        if (!switchSender.IsToggled)
+        {
+            DisplayAlert("It worked", "Switch is Toggled OFF", "OK");   //testing
+            switchName = switchSender.AutomationId;
+            stringBooleanMap[switchName] = false;
+        }
+    }
 }
